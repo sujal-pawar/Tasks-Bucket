b3ca8e27b6310259a749c1d3eb7a2f794de2e564
Sujal Pawar <sujalpawar00007@gmail.com>
2024-11-01 19:03:05 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,14 +5,14 @@ const About = () => {
     return (
         <div>
             <div className="container my-5" >
-                <div className="p-5 text-center rounded-3" >
+                <div className="p-5 text-center mb-3  rounded-3" >
                     <h1 className="text-body-emphasis">About Task Bucket</h1>
                     <p className="col-lg-8 mx-auto fs-5 text-muted">
-                        Welcome to Tasks Bucket, your simple yet powerful to-do list app built with React. 
-                        Whether you're managing daily tasks or planning long-term projects, Text Bucket keeps your to-dos organized and accessible.
-                        With its intuitive interface, you can easily add, edit, and delete tasks.
-                        Plus, Tasks Bucket remembers your tasks even when you come back later, ensuring nothing slips through the cracks. 
-                        Stay on top of your tasks and make your life a little less hectic with Tasks Bucket.
+                       Welcome to Tasks Bucket, your simple yet powerful to-do list app built with React. 
+                       Whether you're managing daily tasks or planning long-term projects, 
+                       Tasks Bucket keeps your to-dos organized and accessible. With its intuitive interface, you can easily add and delete tasks. 
+                       Even if you refresh the page, your tasks won’t get lost—Tasks Bucket saves them so you can see your pending tasks when you come back to the site.
+                       Stay on top of your tasks and make your life a little less hectic with Tasks Bucket.
                     </p>
                 </div>
             </div>
