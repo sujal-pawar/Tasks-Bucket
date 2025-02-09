c43a836dd05790e78c2257ea7acae9ea3a9f4ace
Sujal Pawar <sujalpawar00007@gmail.com>
2025-02-09 18:06:45 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <div>
             <div className="container ">
                 <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                    <p className="col-md-4 mb-0 text-body-dark">© 2024 Tasks Bucket</p>
+                    <p className="col-md-4 mb-0 text-body-dark">© 2025 Tasks Bucket</p>
                 </footer>
             </div>
         </div>
